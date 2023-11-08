ea6c98630ae52a2fb102b31e6619a1140f6406d5
BrahamGont <141289241+BrahamGont@users.noreply.github.com>
2023-11-07 20:18:13 -0500

    Aumento Tabla Admin

--- proyectoeconomia.sql
+++ proyectoeconomia.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-11-2023 a las 13:33:16
+-- Tiempo de generación: 08-11-2023 a las 02:16:50
 -- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.0.25
+-- Versión de PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,19 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyectoeconomia`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `clave` varchar(255) NOT NULL,
+  `intituciones_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -98,6 +111,13 @@ CREATE TABLE `tipos_de_creditos` (
 --
 
 --
+-- Indices de la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `intituciones_admin` (`intituciones_admin`);
+
+--
 -- Indices de la tabla `cobros_indirectos`
 --
 ALTER TABLE `cobros_indirectos`
@@ -168,6 +188,12 @@ ALTER TABLE `tipos_de_creditos`
 --
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`intituciones_admin`) REFERENCES `instituciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `cobros_indirectos`
