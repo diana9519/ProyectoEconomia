d3aec8b1c94a144103f5d9d053b757ff4086497a
diana9519 <94394943+diana9519@users.noreply.github.com>
2023-11-07 20:13:20 -0500

    cálculo de amortización

---
+++ models/LoanModel.js
@@ -0,0 +1,12 @@
+class LoanModel {
+    constructor() {
+      this.montoPrestamo = 0;
+      this.tasaInteres = 0;
+      this.plazoPrestamo = 0;
+    }
+  
+    calcularAmortizacion() {
+    }
+  }
+  
+  
